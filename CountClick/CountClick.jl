116d28795c6d5a81f1488e0a38706d5f6e858bd4
goropikari <goropikari@users.noreply.github.com>
2018-01-01 15:15:27 +0900

    first commit

---
+++ CountClick/CountClick.jl
@@ -0,0 +1,21 @@
+using Gtk.ShortNames, Gtk
+
+win = GtkWindow("Sample App")
+v = GtkBox(:v)
+push!(win, v)
+
+l = GtkLabel("You clicked 0 times.")
+b = GtkButton("Press me")
+push!(v, l)
+push!(v, b)
+
+niter = 0
+function click(widget)
+    global niter += 1
+    setproperty!(l, :label, "You clicked $niter times.")
+    return nothing
+end
+
+signal_connect(click, b, "clicked")
+
+showall(win)
