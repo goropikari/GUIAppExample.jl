98bc3522a1b47ce215f65815296452a589fe5724
goropikari <goropikari@users.noreply.github.com>
2018-01-01 15:18:49 +0900

    first commit

---
+++ CountClick/CountClickandReset.jl
@@ -0,0 +1,31 @@
+using Gtk.ShortNames, Gtk
+
+win = GtkWindow("Sample App")
+v = GtkBox(:v)
+push!(win, v)
+
+l = GtkLabel("You clicked 0 times.")
+r = GtkButton("Reset")
+b = GtkButton("Press me")
+push!(v, l)
+push!(v, r)
+push!(v, b)
+
+niter = 0
+function click(widget)
+    global niter += 1
+    setproperty!(l, :label, "You clicked $niter times.")
+    return nothing
+end
+
+function resetcount(widget)
+    global niter = 0
+    setproperty!(l, :label, "You clicked 0 times.")
+    return nothing
+end
+
+signal_connect(click, b, "clicked")
+signal_connect(resetcount, r, "clicked")
+
+
+showall(win)
